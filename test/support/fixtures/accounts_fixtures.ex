79ac2634a7fbcd1f8c178c936ce54d1d6a21c21b
yohyama <13836268+yohyama0216@users.noreply.github.com>
2024-04-26 14:28:31 +0000

    Initial commit

---
+++ test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,20 @@
+defmodule PhoenixApp.AccountsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `PhoenixApp.Accounts` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        name: "some name"
+      })
+      |> PhoenixApp.Accounts.create_user()
+
+    user
+  end
+end
