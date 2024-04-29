1f556e71c2c03c53e21cb966b02362a39b9e2a33
yohyama <13836268+yohyama0216@users.noreply.github.com>
2024-04-29 15:02:33 +0000

    デザイン調整（フォントアイコン追加）　状態を増やした

--- lib/phoenix_app_web/helpers/status_helper.ex
+++ lib/phoenix_app_web/helpers/status_helper.ex
@@ -1,9 +1,11 @@
 defmodule PhoenixAppWeb.Helpers.StatusHelper do
   def status_label(status_code) do
     case status_code do
-      1 -> "現職"
-      2 -> "休職"
-      _ -> "退職"
+      1 -> "入社予定"
+      2 -> "現職"
+      3 -> "休職"
+      4 -> "退職予定"
+      0 -> "退職済み"
     end
   end
 end
